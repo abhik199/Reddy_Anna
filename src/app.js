d1d797222d6e87124805eb9b1c11f9716126f171
abhik199 <abhishekkirar2004@gmail.com>
2025-01-11 22:33:53 +0530

    setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 9600;
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
