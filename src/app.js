eac3a9bcfc757daa3c426d9df06bbac9e880a9fd
abhik199 <abhishekkirar2004@gmail.com>
2025-01-25 01:21:56 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "../public/dist")));
+// Set EJS as the template engine
+app.set("view engine", "ejs");
 
+// Set the views directory (optional, defaults to './views')
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define a route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/dist", "index.html"));
+  res.render("index", { title: "Welcome to EJS", message: "Hello, EJS!" });
 });
 
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
